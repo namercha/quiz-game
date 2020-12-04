7fe633e4439a5546f6fc0ec844efed99ca8e055e
nabil <merchant.nabil@gmail.com>
2020-12-04 04:34:16 -0600

    Adding main.py code.

---
+++ main.py
@@ -0,0 +1,9 @@
+from question_model import Question
+from data import question_data
+
+question_bank = []
+for item in question_data:
+    question_text = item["text"]
+    question_answer = item["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
